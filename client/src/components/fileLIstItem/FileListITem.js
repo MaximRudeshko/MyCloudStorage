01f88d31d6d29f48bb9e5812d254b984e76484aa
Maxim <Rudeshkomaximsergeevich@gmail.com>
2020-11-19 18:30:29 +0300

    added util for transformation file's size and add custom scroll bar form list with files

--- client/src/components/fileLIstItem/FileListITem.js
+++ client/src/components/fileLIstItem/FileListITem.js
@@ -9,6 +9,7 @@ import trashSvg from '../../assets/img/trash.svg'
 
 import './fileListItem.scss'
 import { removeFileFRomUploader } from '../../redux/actions/uploader';
+import sizeFormat from '../../utils/sizeFormat';
 
 const FileListITem = ({file}) => {
     const dispatch = useDispatch()
@@ -36,7 +37,7 @@ const FileListITem = ({file}) => {
             <img src = {file.type === 'dir' ? folder : fileSvg } alt = 'file-pic' className = 'file__img' />
             <div className = 'file__name'>{file.name}</div>
             <div className = 'file__date'>{file.date.slice(0, 10)}</div>
-            <div className = 'file__size'>{file.size} Kb</div>
+            <div className = 'file__size'>{sizeFormat(file.size)}</div>
             {file.type !== 'dir' && <img src = {downloadSvg}  onClick = {e =>downloadFileHandler(e)} alt = 'download' className = 'file__btn'/>}
             <img src = {trashSvg} onClick = {e => deleteFileHandler(e)} alt = 'trash' className = 'file__btn'/>
         </div>
--- client/src/components/fileList/FileList.js
+++ client/src/components/fileList/FileList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import FileListItem from '../fileLIstItem/FileListITem';
 import {CSSTransition, TransitionGroup} from "react-transition-group";
+import {Scrollbars} from 'react-custom-scrollbars'
 
 import './fileList.scss'
 
@@ -10,25 +11,31 @@ const FileList = () => {
 
     const files = useSelector(state => state.files.files)  
     return ( 
-        <div className = 'filelist'>
-            <div className = 'filelist__header'>
-                <div className="filelist__name">Название</div>
-                <div className="filelist__date">Дата</div>
-                <div className="filelist__size">Размер</div>
+        <Scrollbars autoHide>
+        <div className = 'filelist__wrapper'>
+            <div className = 'filelist'>
+                <div className = 'filelist__header'>
+                    <div className="filelist__name">Название</div>
+                    <div className="filelist__date">Дата</div>
+                    <div className="filelist__size">Размер</div>
+                </div>
+                
+                <TransitionGroup>
+                    {files.map(file =>
+                        <CSSTransition
+                            key={file._id}
+                            timeout={500}
+                            classNames={'file'}
+                            exit={false}
+                        >
+                            <FileListItem file={file}/>
+                        </CSSTransition>
+                    )}
+                </TransitionGroup>
+                
             </div>
-            <TransitionGroup>
-                {files.map(file =>
-                    <CSSTransition
-                        key={file._id}
-                        timeout={500}
-                        classNames={'file'}
-                        exit={false}
-                    >
-                        <FileListItem file={file}/>
-                    </CSSTransition>
-                )}
-            </TransitionGroup>
         </div>
+        </Scrollbars>
     );
 }
 
